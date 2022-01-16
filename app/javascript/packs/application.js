c6d01d66f68e0bb312d04eeca8e1492945d9dae7
fumtas1k <fumta01a7@gmail.com>
2022-01-16 14:48:29 +0900

    add:jqueryでslideToggleを設定

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,3 +4,10 @@ require("@rails/activestorage").start()
 require("channels")
 import "bootstrap";
 import "../stylesheets/application";
+
+$(document).on('turbolinks:load', function() {
+  $("#mini-menu-wrapper").hide();
+  $("#mini-menu-btn").on("click", function(){
+    $("#mini-menu-wrapper").slideToggle(50);
+  });
+ });
