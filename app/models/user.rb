60db84ae2b012ce2c5a49563579ba23511aa9515
fumtas1k <fumta01a7@gmail.com>
2022-01-16 15:52:09 +0900

    improve:emailのvalidationを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true,
-            format: {with: /\A[\w.!#$%&'*+\/=?^`{|}~-]+@[\w-]+(?:\.[\w-]+)*\z/i }
+            format: {with: /\A[\w.!#$%&'*+\/=?^`{|}~-]+@[\w-]+\.[\w-]+\z/i }
   before_validation {email.downcase!}
   has_secure_password
   validates :password, length: {minimum: 6}
