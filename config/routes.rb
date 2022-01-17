49635bb54f31b43a7db43e6ad9ab6db02cce7151
fumtas1k <fumta01a7@gmail.com>
2022-01-17 12:30:11 +0900

    add:favorite機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "pictures#index"
+  resources :favorites, only: %i[create destroy]
   resources :users, only: %i[new index show create destroy]
   resources :sessions, only: %i[new create destroy]
   resources :pictures do
